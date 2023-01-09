1b18136ebe8cb7e1f60dfc294005244f7c0f20ed
imhele <work@imhele.com>
2023-01-10 01:24:41 +0800

    perf(ioc): 移除依赖时，识别循环依赖 & 更新模块文档说明

--- core/ioc/dependency.ts
+++ core/ioc/dependency.ts
@@ -1,7 +1,79 @@
 /**
- * 此模块实现了 class Dependency 用于管理实例的依赖关系。
+ * # 依赖管理
  *
- * 共享的弱引用依赖可以自动回收，而强引用依赖需要手动回收。
+ * 此模块实现了通用的 class Dependency 用于管理依赖关系。
+ *
+ * ## 简介
+ *
+ * Dependency 中同时储存了挂载树与引用关系两套数据，其中**挂载树顾名思义是一颗树、而引用关系则是有向连通图**。
+ *
+ * 简单情况下，不需要共享依赖时，依赖的引用关系应当和挂载树同构，例如：
+ *
+ * ```plain
+ *  ROOT
+ *  /  \
+ * A    B
+ * ```
+ *
+ * 当存在共享依赖时，依赖的引用关系会复杂一些，例如下方示例中，左侧时挂载树、右侧是引用关系：
+ *
+ * ```plain
+ *   RO_OT          RO_OT
+ *  /  |  \         /   \
+ * A   C   B       A     B
+ *                  \   /
+ *                    C
+ * ```
+ *
+ * 上面的例子比较抽象，我们将它转换成具体的例子：可以假设 ROOT 为 JS 服务端程序的一个 Controller
+ * 实例，而 A B C 均为 Services ，按右侧的引用关系解释则是 Controller 引用了 Service A
+ * 与 B 、而 Service A 与 B 在 Controller 存活期间共享了 Service C 。假如 Service C
+ * 不共享，则是下图所示的挂载树与引用关系：
+ *
+ * ```plain
+ *  RO_OT        RO_OT
+ *  /   \        /   \
+ * A     B      A     B
+ * |     |      |     |
+ * C1    C2     C1    C2
+ * ```
+ *
+ * ## 名词约定
+ *
+ * ```plain
+ *   RO_OT          RO_OT
+ *  /  |  \         /   \
+ * A   C   B       A     B
+ *                  \   /
+ *                    C
+ * ```
+ *
+ * 以上图为例，我们有如下说法：
+ *
+ * - 对于左侧的挂载树：
+ *   - ROOT 是挂载树的_根节点_；
+ *   - ROOT 是 A / B / C 的_祖先节点_，同时也是它们的_父节点_；
+ *     ⇔ A / B / C 是 ROOT 的_后代节点_，同时也是它的_子节点_；
+ *   - ROOT _安装_了 A 和 B ；
+ *   - A / B 将共享依赖 C _安装_在了 ROOT 下；
+ *   - A 与 B 可在 ROOT _范围_下共享依赖 C ；
+ * - 对于右侧的引用关系：
+ *   - ROOT _引用_了 A 和 B ；
+ *     ⇔ ROOT _依赖_了 A 和 B ；
+ *     ⇔ ROOT 有两个_依赖项_ A 和 B ；
+ *     ⇔ A / B 存在一个_依赖方_ ROOT ；
+ *   - A / B _引用_了 C ；
+ *     ⇔ A / B _依赖_了 C ；
+ *     ⇔ A / B 有一个_依赖项_ C ；
+ *     ⇔ C 存在两个_依赖方_ A 和 B ；
+ *
+ * 此外，引用关系图中出现_回路_时，也可具体称为出现_循环依赖_。
+ *
+ * ## 使用限制
+ *
+ * 当你通过 `new Dependency(init)` 创建一个 Dependency 实例时，此实例一定是挂载树的根节点；换句话说，你无法通过
+ * `new Dependency(init)` 创建子依赖之后连通到已经存在的挂载树与引用关系图中。不用担心，
+ * Dependency 提供了足够可靠的两个公共方法 link() 与 unlink() ，分别用于加载与移除子依赖。
  *
  * @module
  */
@@ -32,6 +104,9 @@ export interface DependencyInit<Scope, Value = unknown> {
   readonly value?: Value;
 }
 
+/**
+ * 用于管理依赖关系的通用类。
+ */
 export class Dependency<Key, Scope, Value = unknown> {
   /**
    * 安装在当前节点的依赖。
@@ -57,6 +132,14 @@ export class Dependency<Key, Scope, Value = unknown> {
    * 当前节点保存的值。
    */
   #value: Value | undefined;
+
+  /**
+   * 当前节点是否为根节点。
+   */
+  get #isRoot(): boolean {
+    // 没有被回收 && 不存在 #parent
+    return !revoke.has(this) && !this.#parent;
+  }
 
   constructor(init: DependencyInit<Scope, Value>) {
     this.#scope = init.scope === undefined ? NeverScope : init.scope;
@@ -102,10 +185,8 @@ export class Dependency<Key, Scope, Value = unknown> {
     return emplaceMap(this.#references, key, {
       insert: () => {
         const reference = this.#lookupAncestor(shareScope).#install(key, init);
-        if (reference !== this) {
-          reference.#referrers ??= new Set();
-          reference.#referrers.add(this);
-        }
+        reference.#referrers ??= new Set();
+        reference.#referrers.add(this);
         return reference;
       },
     });
@@ -117,18 +198,20 @@ export class Dependency<Key, Scope, Value = unknown> {
   unlink(key: Key): void {
     revoke.assert(this);
 
-    const root = this.#references?.get(key);
-    if (!root) return;
+    const startNode = this.#references?.get(key);
+    if (!startNode) return;
 
     this.#references!.delete(key);
-    root.#referrers?.delete(this);
-    if (root.#referrers?.size) return;
+    startNode.#referrers?.delete(this);
+    if (!startNode.#isUnreachable()) return;
 
     const pending: Iterable<Dependency<Key, Scope>>[] = [];
-    let iterable: Iterable<Dependency<Key, Scope>> | undefined = [root];
+    let iterable: Iterable<Dependency<Key, Scope>> | undefined = [startNode];
 
     do {
       for (const dep of iterable) {
+        // 调用 revoke(dep) 会立即将 dep 标记为已回收，
+        // 因此不会在后续 #isUnreachable() 遍历 referrers 时被误判为根节点
         if (!revoke(dep)) continue;
 
         if (dep.#parent) {
@@ -140,7 +223,7 @@ export class Dependency<Key, Scope, Value = unknown> {
           const unreachable: Dependency<Key, Scope>[] = [];
           for (const reference of dep.#references.values()) {
             reference.#referrers?.delete(dep);
-            if (!reference.#referrers?.size) unreachable.push(reference);
+            if (reference.#isUnreachable()) unreachable.push(reference);
           }
 
           if (unreachable.length) pending.push(unreachable);
@@ -174,6 +257,32 @@ export class Dependency<Key, Scope, Value = unknown> {
         return child;
       },
     });
+  }
+
+  /**
+   * 当前节点是否无法溯源到根节点，如果无法回溯，说明当前节点需要被回收。
+   */
+  #isUnreachable(): boolean {
+    // 已经被回收了
+    if (revoke.has(this)) return true;
+
+    const pending: Iterable<Dependency<Key, Scope>>[] = [];
+    const visited = new WeakSet<Dependency<Key, Scope>>();
+    let iterable: Iterable<Dependency<Key, Scope>> | undefined = [this];
+
+    do {
+      for (const referrer of iterable) {
+        if (visited.has(referrer)) continue;
+        visited.add(referrer);
+
+        // 已到达根节点，说明依然存在存活的依赖引用了当前节点
+        // 如果节点已被回收，则 #isRoot 一定为假
+        if (referrer.#isRoot) return false;
+        if (referrer.#referrers?.size) pending.push(referrer.#referrers);
+      }
+    } while ((iterable = pending.pop()));
+
+    return true;
   }
 
   /**
