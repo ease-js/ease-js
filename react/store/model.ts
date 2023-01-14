d267aaca86a1b0e3fdbd0c15178fb45ac3e30d21
imhele <work@imhele.com>
2023-01-14 18:29:26 +0800

    perf(ioc): 将 useModel 融合到 Model 的静态属性上，调用时无需手动传入参数

--- react/store/model.ts
+++ react/store/model.ts
@@ -9,6 +9,27 @@ import { useDependencyHost } from "./context.ts";
 // deno-lint-ignore no-explicit-any
 export abstract class Model<State extends { readonly [key: string]: any }>
   extends BehaviorSubject<State> {
+  // deno-lint-ignore no-explicit-any
+  static useModel<T extends Model<any>>(
+    this: NewableDependencyKey<[], T>,
+  ): [state: T["value"], model: T] {
+    const host = useDependencyHost();
+    const model = useConstant(() => host.new(this));
+
+    const getSnapshot = useConstant(() => {
+      return (): T["value"] => model.getValue();
+    });
+    const subscribe = useConstant(() => {
+      return (onChange: () => void): () => void => {
+        const subscription = model.subscribe(onChange);
+        return () => subscription.unsubscribe();
+      };
+    });
+
+    const state = useSyncExternalStore(subscribe, getSnapshot);
+    return [state, model];
+  }
+
   #draft: readonly [Draft<State>] | undefined;
 
   get draft(): Draft<State> {
@@ -25,25 +46,4 @@ export abstract class Model<State extends { readonly [key: string]: any }>
       if (!Object.is(this.getValue(), value)) this.next(value);
     }
   }
-}
-
-// deno-lint-ignore no-explicit-any
-export function useModel<T extends Model<any>>(
-  key: NewableDependencyKey<[], T>,
-): [state: T["value"], model: T] {
-  const host = useDependencyHost();
-  const model = useConstant(() => host.new(key));
-
-  const getSnapshot = useConstant(() => {
-    return (): T["value"] => model.getValue();
-  });
-  const subscribe = useConstant(() => {
-    return (onChange: () => void): () => void => {
-      const subscription = model.subscribe(onChange);
-      return () => subscription.unsubscribe();
-    };
-  });
-
-  const state = useSyncExternalStore(subscribe, getSnapshot);
-  return [state, model];
 }
