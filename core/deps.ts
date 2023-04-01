8ecd79b41fa5d1686ab689ee8577e9d545ae913f
imhele <work@imhele.com>
2023-04-01 16:03:53 +0800

    chore: 更新 lock

--- core/deps.ts
+++ core/deps.ts
@@ -1,4 +1,4 @@
 import "./polyfill/proposal-explicit-resource-management.ts";
 
 export * as asserts from "https://deno.land/std@0.181.0/testing/asserts.ts";
-export * as rxjs from "https://esm.sh/rxjs@7.8.0?bundle";
+export * as rxjs from "https://esm.sh/rxjs@7.8.0?bundle&pin=v113";
